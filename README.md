7db36a6ec9cfce4e108ca90528cdc95f3a828d85
Artyom Pervukhin <github@artyom.dev>
2021-06-30 00:26:26 +0300

    Convert from YAML to JSON if invoked as yaml2json
    While here, also make it possible to read input directly from the file provided
    as the first argument, instead of always reading from stdin.

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Command json2yaml converts JSON documents to YAML
 If you have Go installed, install this command with:
 
     go install github.com/artyom/json2yaml@latest
+
+If command is invoked as `yaml2json`, it converts in the opposite direction — from YAML to JSON.
+If your Go binaries are installed under the default $GOPATH/bin, then you can create a symlink like this:
+
+    ln -s -v json2yaml $(go env GOPATH)/bin/yaml2json
--- main.go
+++ main.go
@@ -1,4 +1,6 @@
-// Command json2yaml converts JSON documents to YAML
+// Command json2yaml converts JSON documents to YAML.
+//
+// If command is called by yaml2json name, then it converts from YAML to JSON.
 package main
 
 import (
@@ -8,6 +10,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 
 	"gopkg.in/yaml.v3"
 )
@@ -15,14 +18,30 @@ import (
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
-	if err := run(); err != nil {
+	if err := run(flag.Arg(0)); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run() error {
-	dec := json.NewDecoder(os.Stdin)
-	enc := yaml.NewEncoder(os.Stdout)
+func run(name string) error {
+	input, output := os.Stdin, os.Stdout
+	if name != "" && name != "-" {
+		f, err := os.Open(name)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		input = f
+	}
+	if reverseMode {
+		return yaml2json(input, output)
+	}
+	return json2yaml(input, output)
+}
+
+func json2yaml(input io.Reader, output io.Writer) error {
+	dec := json.NewDecoder(input)
+	enc := yaml.NewEncoder(output)
 	defer enc.Close()
 	for {
 		var doc interface{}
@@ -38,8 +57,35 @@ func run() error {
 	}
 }
 
+func yaml2json(input io.Reader, output io.Writer) error {
+	dec := yaml.NewDecoder(input)
+	enc := json.NewEncoder(output)
+	enc.SetEscapeHTML(false)
+	enc.SetIndent("", "  ")
+	for {
+		var doc interface{}
+		if err := dec.Decode(&doc); err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+		if err := enc.Encode(doc); err != nil {
+			return err
+		}
+	}
+}
+
+var reverseMode = len(os.Args) != 0 && filepath.Base(os.Args[0]) == "yaml2json"
+
 func init() {
 	flag.Usage = func() {
-		fmt.Fprintln(flag.CommandLine.Output(), "Usage: json2yaml < input.json > output.yaml")
+		if reverseMode {
+			fmt.Fprintln(flag.CommandLine.Output(), "Usage: yaml2json < input.yaml > output.json\n"+
+				"   or: yaml2json input.yaml > output.json")
+		} else {
+			fmt.Fprintln(flag.CommandLine.Output(), "Usage: json2yaml < input.json > output.yaml\n"+
+				"   or: json2yaml input.json > output.yaml")
+		}
 	}
 }
